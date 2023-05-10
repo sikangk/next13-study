d67f99053c5361c400064a7759520dc26bcc0cc7
sikangk <updream4@naver.com>
2023-05-10 16:49:14 +0900

    정적라우팅

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function ContactPage(){
+    return(
+        <h1>contact page</h1>
+    )
+}
---
+++ src/app/products/pants/page.tsx
@@ -0,0 +1,5 @@
+export default function Pants(){
+    return(
+        <h1>Pants</h1>
+    )
+}
