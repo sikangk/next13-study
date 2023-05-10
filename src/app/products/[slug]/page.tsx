a9241e15981b7ec9ead392f916c05b39d383c4b7
sikangk <updream4@naver.com>
2023-05-10 17:25:01 +0900

    동적라우팅 페이지 특정 slug 프리렌더링(ssg)

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -3,10 +3,19 @@ type Props = {
         slug: string;
     }
 }
-function Pants({params}:Props) {
+
+function Pants({params}: Props) {
     return (
         <h1>{params.slug}</h1>
     )
 }
 
+// 특정 slug를 ssg로 하려면
+export function generateStaticParams() {
+    const products = ['pants', 'skirt'];
+    return products.map(product => ({slug: product}))
+}
+
 export default Pants;
+
+
