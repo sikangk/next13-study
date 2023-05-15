31aa1cd42633eb0ec6a906b0cfb8067e270169d1
sikangk <updream4@naver.com>
2023-05-15 19:36:03 +0900

    products 페이지네 요소 Link

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,18 @@
-export default function Products(){
-    return(
-        <h1>Products</h1>
+import Link from "next/link";
+
+const products = ['shirt', 'pants', 'skirt', 'shoes'];
+
+export default function Products() {
+    return (
+        <>
+            <h1>제품 소개 페이지</h1>
+            <ul>
+                {
+                    products.map((product, index) => <li key={index}>
+                        <Link href={`products/${product}`}>{product}</Link>
+                    </li>)
+                }
+            </ul>
+        </>
     )
 }
