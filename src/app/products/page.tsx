beadd05da7fc78a6beaca603f46c975c1475f24c
sikangk <updream4@naver.com>
2023-05-23 15:52:51 +0900

    SSR의 개념 fetch

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
 import {getProducts} from "@/service/protucts";
+import styles from './page.module.css';
 
 
-export const revalidate = 3;
+//export const revalidate = 3;
 
 export default async function Products() {
     const products = await getProducts();
+    const res = await fetch('https://meowfacts.herokuapp.com',{
+        next:{revalidate:3},
+    });
+    const data = await res.json();
+    const factText = data.data[0];
     return (
         <>
             <h1>제품 소개 페이지</h1>
@@ -16,6 +22,9 @@ export default async function Products() {
                     </li>)
                 }
             </ul>
+            <article className={styles.article}>
+                {factText}
+            </article>
         </>
     )
 }
