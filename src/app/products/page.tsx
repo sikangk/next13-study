8aa73ea0fa95c893541d72a8c14e007d278d5552
sikangk <updream4@naver.com>
2023-05-23 16:07:07 +0900

    CSR의 개념 클라이언트 컴포넌트 fetch

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,12 @@
 import Link from "next/link";
 import {getProducts} from "@/service/protucts";
-import styles from './page.module.css';
+import MeowArticle from "@/componenets/MeowArticle";
 
 
 //export const revalidate = 3;
 
 export default async function Products() {
     const products = await getProducts();
-    const res = await fetch('https://meowfacts.herokuapp.com',{
-        next:{revalidate:3},
-    });
-    const data = await res.json();
-    const factText = data.data[0];
     return (
         <>
             <h1>제품 소개 페이지</h1>
@@ -22,9 +17,7 @@ export default async function Products() {
                     </li>)
                 }
             </ul>
-            <article className={styles.article}>
-                {factText}
-            </article>
+            <MeowArticle/>
         </>
     )
 }
---
+++ src/componenets/MeowArticle.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, {useState, useEffect} from 'react';
+import styles from "./MeowArticle.module.css";
+
+function MeowArticle() {
+    const [text, setText] = useState('데이터 준비중 ...');
+
+
+    useEffect(() => {
+        fetch('https://meowfacts.herokuapp.com', {
+            next: {revalidate: 0},
+        }).then((res) => res.json())
+            .then((data) => setText(data.data[0]))
+
+    }, [])
+
+    return (
+        <article className={styles.article}>
+            {text}
+        </article>
+    )
+
+}
+
+export default MeowArticle;
